763c421c50be6d298dafce764f43181460d7ccaa
RaidenXVR <fitran.nizar@gmail.com>
2024-06-28 11:25:07 +0700

    changed webui-user.sh

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+export COMMANDLINE_ARGS="--api --nowebui --port 3000 --no-download-sd-model --skip-torch-cuda-test --enable-insecure-extension-access"
 
 # python3 executable
 #python_cmd="python3"
@@ -19,13 +19,13 @@
 #export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-#venv_dir="venv"
+venv_dir="/sdforge/venv"
 
 # script to launch to start the app
-#export LAUNCH_SCRIPT="launch.py"
+export LAUNCH_SCRIPT="/sdforge/launch.py"
 
 # install command for torch
-#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+#export TORCH_COMMAND="pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu125"
 
 # Requirements file to use for stable-diffusion-webui
 #export REQS_FILE="requirements_versions.txt"
